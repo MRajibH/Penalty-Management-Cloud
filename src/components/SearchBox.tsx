5b9888577a6456e8873b939903f1cdd6c19a5f3c
Polinkhan <polinsir@gmail.com>
2025-01-15 00:15:47 +0600

    added searchbar

---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,78 @@
+import { Button } from "./ui/button";
+import {
+  Calculator,
+  Calendar,
+  CreditCard,
+  Search,
+  Settings,
+  Smile,
+  User,
+} from "lucide-react";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "./ui/command";
+import { useState } from "react";
+
+const SearchBox = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="min-w-[300px] px-2">
+      <Button
+        variant={"ghost"}
+        onClick={() => setOpen(true)}
+        className="w-full text-gray-500 justify-start border bg-white hover:bg-white hover:shadow-sm"
+      >
+        <Search />
+        Search here ...
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>
+              <Calendar />
+              <span>Calendar</span>
+            </CommandItem>
+            <CommandItem>
+              <Smile />
+              <span>Search Emoji</span>
+            </CommandItem>
+            <CommandItem>
+              <Calculator />
+              <span>Calculator</span>
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>
+              <User />
+              <span>Profile</span>
+              <CommandShortcut>⌘P</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <CreditCard />
+              <span>Billing</span>
+              <CommandShortcut>⌘B</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <Settings />
+              <span>Settings</span>
+              <CommandShortcut>⌘S</CommandShortcut>
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </div>
+  );
+};
+
+export default SearchBox;
